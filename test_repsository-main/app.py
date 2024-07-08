4c925badd5800943f647695b3a6387db3c81f9ab
Dr Health Bot Program <info@drbot.health>
2024-07-08 19:48:26 -0400

    Update app.py
    updated the app

--- test_repsository-main/app.py
+++ test_repsository-main/app.py
@@ -59,6 +59,4 @@ def thank_you():
 
 # Run the app
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
-    app.run(debug=True)
+    app.run(debug=False, host='0.0.0.0')
